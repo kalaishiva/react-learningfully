b208c5d8467765f4124b40c41a6c4d30d8cbd08a
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-23 17:37:45 +0200

    props and default props usage is learnt

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 
-const Header = () => {
+const Header = ({name, subject}) => {
   return (
+    <>
     <h1>Task Tracker</h1>
+    <h2>Gives the Name : {name}</h2>
+    <h2>Gives the Subject : {subject}</h2>
+    </>
   )
+}
+
+//set the default props 
+
+Header.defaultProps = {
+    name: "Kalai"
 }
 
 export default Header
