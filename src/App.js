df41fe84968b2e09ddaea2577eda510e491126fc
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-25 09:28:41 +0200

    task is added to the list

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
        }
 ])
 
+//Add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000)+1 ;
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //delete the task
 const deleteTask=(id)=>{
  setTasks(tasks.filter((task)=>task.id !== id))
@@ -40,7 +47,7 @@ const toggleReminder = (id)=>{
   return (
     <div className="App">
         <Header />
-        <AddTask />
+        <AddTask onAdd ={addTask}/>
         { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Task to show"
         }      
     </div>
